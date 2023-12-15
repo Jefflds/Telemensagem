d297bcb821750ca0f872a7da5a11f5eff7970c2b
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-15 03:31:47 +0000

    criacao rotas aniversario

--- src/routes/Aniversario/AniversarioRoutes.tsx
+++ src/routes/Aniversario/AniversarioRoutes.tsx
@@ -1,6 +1,45 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Aniversario from "./Aniversario";
+import AmigaAmigo from "./AmigaAmigo/AmigaAmigo"; 
+import Neutro from "./Neutro/Neutro";
+import PessoaEspecial from "./PessoaEspecial/PessoaEspecial";
+import Atrasado from "./Atrasado/Atrasado"; 
+import Antecipado from "./Antecipado/Antecipado"; 
+import ComGozacao from "./ComGozacao/ComGozacao";
+import ComOtimismo from "./ComOtimismo/ComOtimismo"; 
+import PessoaDistante from "./PessoaDistante/PessoaDistante"; 
+import Cliente from "./Cliente/Cliente"; 
+import Crianca from "./Crianca/Crianca"; 
+import QuinzeAnos from "./QuinzeAnos/QuinzeAnos"; 
+import Casamento from "./Casamento/Casamento"; 
+import Terceiros from "./Terceiros/Terceiros";
+import MaePai from "./MaePai/MaePai";
+import FilhoFilha from "./FilhoFilha/FilhoFilha";
+import IrmaoIrma from "./irmaoirma/irmaoirma";
+import SograSogro from "./sograsogro/sograsogro";
+import GenroNora from "./genronora/genronora";
+import CunhadaCunhado from "./cunhadacunhado/cunhadacunhado";
+import TiaTio from "./tiatio/tiatio";
+import SobrinhaSobrinho from "./sobrinhasobrinho/sobrinhasobrinho";
+import PrimaPrimo from "./cunhadacunhado/primaprimo";
+import AvoAvo from "./avoavo/avoavo";
+import NetaNeto from "./netaneto/netaneto";
+import MadrinhaPadrinho from "./madrinhapadrinho/madrinhapadrinho";
+import AfilhadaAfilhado from "./afilhadaafilhado/afilhadaafilhado";
+import ComadreCompadre from "./comadrecompadre/comadrecompadre";
+import NamoradaNamorado from "./namoradanamorado/namoradanamorado";
+import Ficantes from "./ficantes/ficantes";
+import EsposaEsposo from "./esposaesposo/esposaesposo";
+import NoivaNoivo from "./noivanoivo/noivanoivo";
+import ExNamoradoExEsposo from "./exnamoradoaexesposoa/exnamoradoaexesposoa";
+import CasamentoRota from "./casamento/casamento";
+import Noivado from "./noivado/noivado";
+import Namoro from "./namoro/namoro";
+import Amante from "./amante/amante";
+import Reconciliacao from "./reconciliacao/reconciliacao";
+import DeRelacionamento from "./derelacionamento/derelacionamento";
+import ComConquista from "./comconquista/comconquista";
 
 const layoutsItems = [
   {
@@ -68,6 +107,45 @@ const AniversarioRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Aniversario layoutsItems={layoutsItems} />} />
+      <Route path="/amigaamigo" element={<AmigaAmigo />} />
+      <Route path="/neutro" element={<Neutro />} />
+      <Route path="/pessoaespecial" element={<PessoaEspecial />} />
+      <Route path="/atrasado" element={<Atrasado />} />
+      <Route path="/antecipado" element={<Antecipado />} />
+      <Route path="/comgozacao" element={<ComGozacao />} />
+      <Route path="/comotimismo" element={<ComOtimismo />} />
+      <Route path="/pessoadistante" element={<PessoaDistante />} />
+      <Route path="/cliente" element={<Cliente />} />
+      <Route path="/crianca" element={<Crianca />} />
+      <Route path="/15anos" element={<QuinzeAnos />} />
+      <Route path="/casamento" element={<Casamento />} />
+      <Route path="/terceiros" element={<Terceiros />} />
+      <Route path="/maepai" element={<MaePai />} />
+      <Route path="/filhofilha" element={<FilhoFilha />} />
+      <Route path="/irmaoirma" element={<IrmaoIrma />} />
+      <Route path="/sograsogro" element={<SograSogro />} />
+      <Route path="/genronora" element={<GenroNora />} />
+      <Route path="/cunhadacunhado" element={<CunhadaCunhado />} />
+      <Route path="/tiatio" element={<TiaTio />} />
+      <Route path="/sobrinhasobrinho" element={<SobrinhaSobrinho />} />
+      <Route path="/primaprimo" element={<PrimaPrimo />} />
+      <Route path="/avoavo" element={<AvoAvo />} />
+      <Route path="/netaneto" element={<NetaNeto />} />
+      <Route path="/madrinhapadrinho" element={<MadrinhaPadrinho />} />
+      <Route path="/afilhadaafilhado" element={<AfilhadaAfilhado />} />
+      <Route path="/comadrecompadre" element={<ComadreCompadre />} />
+      <Route path="/namoradanamorado" element={<NamoradaNamorado />} />
+      <Route path="/ficantes" element={<Ficantes />} />
+      <Route path="/esposaesposo" element={<EsposaEsposo />} />
+      <Route path="/noivanoivo" element={<NoivaNoivo />} />
+      <Route path="/exnamoradoaexesposoa" element={<ExNamoradoExEsposo />} />
+      <Route path="/casamento" element={<CasamentoRota />} />
+      <Route path="/noivado" element={<Noivado />} />
+      <Route path="/namoro" element={<Namoro />} />
+      <Route path="/amante" element={<Amante />} />
+      <Route path="/reconciliacao" element={<Reconciliacao />} />
+      <Route path="/derelacionamento" element={<DeRelacionamento />} />
+      <Route path="/comconquista" element={<ComConquista />} />
     </Routes>
   );
 };
