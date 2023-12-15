138c266ea075b550a35b2e353da9f1ed5dd80ef4
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-15 22:42:21 +0000

    Alterando audios

---
+++ src/routes/Aniversario/PessoaEspecial/PessoaEspecial.data.ts
@@ -0,0 +1,81 @@
+// @ts-ignore
+import AudioFile1 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(1).mp3";
+// @ts-ignore
+import AudioFile2 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(2).mp3";
+// @ts-ignore
+import AudioFile3 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(3).mp3";
+// @ts-ignore
+import AudioFile4 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(4).mp3";
+// @ts-ignore
+import AudioFile5 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(5).mp3";
+// @ts-ignore
+import AudioFile6 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(6).mp3";
+// @ts-ignore
+import AudioFile7 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(7).mp3";
+// @ts-ignore
+import AudioFile8 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(8).mp3";
+// @ts-ignore
+import AudioFile9 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(9).mp3";
+// @ts-ignore
+import AudioFile10 from "../../../assets/Msgaudios/Aniversário/Aniversário-Pessoas-especiais/Telemensagem(10).mp3";
+
+
+export const audioData = [
+    {
+        id: "00",
+        title: " Aniversário de Pessoa Especial",
+        subTitle: "",
+        subItems: [
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile1,
+            },
+            {
+                id: "0494M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile2,
+            },
+            {
+                id: "1665M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile3,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile4,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile5,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile6,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile7,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile8,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile9,
+            },
+            {
+                id: "0493M",
+                subTitle: " Aniversário de pessoa especial (voz feminina)",
+                src: AudioFile10,
+            },
+        ],
+    },
+]
