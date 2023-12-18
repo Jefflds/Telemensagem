21ced709a5b75b0b309f7a021d9951564cbfbb7b
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-18 00:24:01 +0000

    Update Audio Data

---
+++ src/routes/Aniversario/Cliente/Cliente.data.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import AudioFile1 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(1).mp3";
+// @ts-ignore
+import AudioFile2 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(2).mp3";
+// @ts-ignore
+import AudioFile3 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(3).mp3";
+// @ts-ignore
+import AudioFile4 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(4).mp3";
+// @ts-ignore
+import AudioFile5 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(5).mp3";
+// @ts-ignore
+import AudioFile6 from "../../../assets/Msgaudios/Aniversario/Aniversario-clientes/Telemensagem(6).mp3";
+
+export const audioData = [
+    {
+      id: "00",
+      title: "Aniversário de cliente",
+      subTitle: "",
+      subItems: [
+        {
+          id: "1015",
+          subTitle: " Aniversário de cliente (voz feminina)",
+          src: AudioFile1,
+        },
+        {
+          id: "1016",
+          subTitle: " Aniversário de cliente (voz masculina)",
+          src: AudioFile2,
+        },
+        {
+          id: "1017",
+          subTitle: " Aniversário de cliente (voz feminina)",
+          src: AudioFile3,
+        },
+        {
+            id: "1018",
+            subTitle: " Aniversário de cliente (voz masculina)",
+            src: AudioFile4,
+          },
+          {
+            id: "1019",
+            subTitle: " Aniversário de cliente (voz feminina)",
+            src: AudioFile5,
+          },
+          {
+            id: "1020",
+            subTitle: " Aniversário de cliente (voz masculina)",
+            src: AudioFile6,
+          },
+      ],
+    },
+  ];
