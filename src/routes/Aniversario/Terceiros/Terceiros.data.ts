c500a31972e866cc372753ad7882a40493db475c
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-18 01:34:32 +0000

    Update Audio Data

---
+++ src/routes/Aniversario/Terceiros/Terceiros.data.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import AudioFile1 from "../../../assets/Msgaudios/Aniversario/Aniversario-para-terceiros/Telemensagem(1).mp3";
+// @ts-ignore
+import AudioFile2 from "../../../assets/Msgaudios/Aniversario/Aniversario-para-terceiros/Telemensagem(2).mp3";
+// @ts-ignore
+import AudioFile3 from "../../../assets/Msgaudios/Aniversario/Aniversario-para-terceiros/Telemensagem(3).mp3";
+// @ts-ignore
+import AudioFile4 from "../../../assets/Msgaudios/Aniversario/Aniversario-para-terceiros/Telemensagem(4).mp3";
+
+
+export const audioData = [
+    {
+        id: "00",
+        title: " Aniversário de Casamento para terceiros",
+        subTitle: "",
+        subItems: [
+            {
+                id: "1074",
+                subTitle: " Aniversário de casamento (voz masculina)",
+                src: AudioFile1,
+            },
+            {
+                id: "1075",
+                subTitle: " Aniversário de casamento (voz feminina)",
+                src: AudioFile2,
+            },
+            {
+                id: "1076",
+                subTitle: " Aniversário de casamento (voz masculina)",
+                src: AudioFile3,
+            },
+            {
+                id: "1077",
+                subTitle: " Aniversário de casamento (voz feminina)",
+                src: AudioFile4,
+            },
+        ],
+    },
+]
--- src/routes/Aniversario/Terceiros/Terceiros.tsx
+++ src/routes/Aniversario/Terceiros/Terceiros.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import PageAudioLayout from "../../../components/PageAudioLayout/PageAudioLayout";
+import { audioData } from "./Terceiros.data";
 
 const Terceiros: React.FC = () => {
-    return <></>
+    return <PageAudioLayout audioData={audioData}/>;
 }
-
 export default Terceiros;
