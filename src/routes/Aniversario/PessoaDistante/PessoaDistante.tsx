a29f204c921e74153984a9788eaada896a8a9aaf
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-18 01:26:36 +0000

    Update Audio Data

--- src/routes/Aniversario/PessoaDistante/PessoaDistante.tsx
+++ src/routes/Aniversario/PessoaDistante/PessoaDistante.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import PageAudioLayout from "../../../components/PageAudioLayout/PageAudioLayout";
+import { audioData } from "./PessoaDistante.data";
 
 const PessoaDistante: React.FC = () => {
-    return <></>
+    return <PageAudioLayout audioData={audioData}/>
 }
 
 export default PessoaDistante;
