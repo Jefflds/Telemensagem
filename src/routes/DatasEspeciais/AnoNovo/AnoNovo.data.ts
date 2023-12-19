111abfa90ce92b107bd7f976bd6fa8474492bd04
Portela03 <joao.portela03@gmail.com>
2023-12-18 21:38:31 -0300

    Adicionando audio d ano novo

---
+++ src/routes/DatasEspeciais/AnoNovo/AnoNovo.data.ts
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-expect-error
+import AudioFile1 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(1).mp3";
+// @ts-ignore
+import AudioFile2 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(2).mp3";
+// @ts-ignore
+import AudioFile3 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(3).mp3";
+// @ts-ignore
+import AudioFile4 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(4).mp3";
+// @ts-ignore
+import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(5).mp3";
+// @ts-ignore
+import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(6).mp3";
+// @ts-ignore
+import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(7).mp3";
+// @ts-ignore
+// // @ts-ignore
+// import AudioFile9 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(9).mp3";
+// // @ts-ignore
+// import AudioFile10 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(10).mp3";
+// // @ts-ignore
+// import AudioFile11 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(11).mp3";
+// // @ts-expect-error
+// import AudioFile12 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(12).mp3";
+// // @ts-ignore
+// import AudioFile13 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(13).mp3";
+// // @ts-ignore
+// import AudioFile14 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(14).mp3";
+// // @ts-ignore
+// import AudioFile15 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(15).mp3";
+// // @ts-ignore
+// import AudioFile16 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(16).mp3";
+// // @ts-ignore
+// import AudioFile17 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(17).mp3";
+// // @ts-ignore
+// import AudioFile18 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(18).mp3";
+// // @ts-ignore
+// import AudioFile19 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(19).mp3";
+// // @ts-ignore
+// import AudioFile20 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(20).mp3";
+// // @ts-ignore
+// import AudioFile21 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(21).mp3";
+
+export const audioData = [
+    {
+      id: "00",
+      title: "Ano Novo",
+      subTitle: "(neutra/distante/namorados/casados/evangélica)",
+      subItems: [
+        {
+          id: "1154",
+          subTitle: " Ano Novo Neutra (voz masculina)",
+          src: AudioFile1,
+        },
+        {
+          id: "0002",
+          subTitle: " 1154-Ano Novo Neutra (voz feminina)",
+          src: AudioFile2,
+        },
+        {
+          id: "0003 M",
+          subTitle: " Aniversário amiga (voz feminina)",
+          src: AudioFile3,
+        },
+        {
+          id: "0004 M",
+          subTitle: " Ano Novo Neutra (voz feminina)",
+          src: AudioFile4,
+        },
+        {
+          id: "0657 M",
+          subTitle: " Ano Novo Neutra (voz feminina)",
+          src: AudioFile5,
+        },
+        {
+          id: "0658 M",
+          subTitle: " Aniversário amiga (voz masculina)",
+          src: AudioFile6,
+        },
+        {
+          id: "0005 M",
+          subTitle: " Ano Novo Neutra (voz masculina)",
+          src: AudioFile7,
+        },
+       
+    //   
+    // {
+    //   id: "01",
+    //   title: "",
+    //   subTitle: "Aniversário Amigo",
+    //   subItems: [
+    //     {
+    //       id: "0011 M",
+    //       subTitle: " Aniversário amigo (voz feminina)",
+    //       src: AudioFile13,
+    //     },
+    //     {
+    //       id: "0012 M",
+    //       subTitle: " Aniversário amigo (voz masculina)",
+    //       src: AudioFile14,
+    //     },
+    //     {
+    //       id: "0013",
+    //       subTitle: " Aniversário amigo (voz feminina)",
+    //       src: AudioFile15,
+    //     },
+    //     {
+    //       id: "0014",
+    //       subTitle: " Aniversário amigo (voz masculina)",
+    //       src: AudioFile16,
+    //     },
+    //     {
+    //       id: "0015",
+    //       subTitle: " Aniversário amigo (voz feminina)",
+    //       src: AudioFile17,
+    //     },
+    //     {
+    //       id: "0016",
+    //       subTitle: " Aniversário amigo (voz masculina)",
+    //       src: AudioFile18,
+    //     },
+    //     {
+    //       id: "0017",
+    //       subTitle: " Aniversário amigo (voz feminina)",
+    //       src: AudioFile19,
+    //     },
+    //     {
+    //       id: "0018",
+    //       subTitle: " Aniversário amigo (voz masculina)",
+    //       src: AudioFile20,
+    //     },
+    //     {
+    //       id: "0019",
+    //       subTitle: " Aniversário amigo (voz feminina)",
+    //       src: AudioFile21,
+    //     },
+    ]
+   }
+ ];
--- src/routes/DatasEspeciais/AnoNovo/AnoNovo.tsx
+++ src/routes/DatasEspeciais/AnoNovo/AnoNovo.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import PageAudioLayout from "../../../components/PageAudioLayout/PageAudioLayout";
+import { audioData } from "./AnoNovo.data";
 
-const AnoNovo = () => {
-  return (
-    <div>AnoNovo</div>
-  )
+const AnoNovo: React.FC = () => {
+    return <PageAudioLayout audioData={audioData}/>
 }
 
-export default AnoNovo
+export default AnoNovo;
