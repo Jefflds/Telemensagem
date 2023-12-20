7650bb15561b2aa291e9d244c4a8525b36079a53
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-20 16:03:32 +0000

    Update Audio Data

---
+++ src/routes/DatasEspeciais/DiaDosProfessores/DiaDosProfessores.data.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-expect-error
+import AudioFile1 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(1).mp3";
+// @ts-expect-error
+import AudioFile2 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(2).mp3";
+// @ts-expect-error
+import AudioFile3 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(3).mp3";
+// @ts-expect-error
+import AudioFile4 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(4).mp3";
+// @ts-expect-error
+import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(5).mp3";
+// @ts-expect-error
+import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(6).mp3";
+// @ts-expect-error
+import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(7).mp3";
+// @ts-expect-error
+import AudioFile8 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(8).mp3";
+// @ts-expect-error
+import AudioFile9 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(9).mp3";
+// @ts-expect-error
+import AudioFile10 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(10).mp3";
+// @ts-expect-error
+import AudioFile11 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-Professores/Telemensagem(11).mp3";
+
+export const audioData = [
+  {
+    id: "00",
+    title: "Dia dos Professores",
+    subTitle: "",
+    subItems: [
+      {
+        id: "1613",
+        subTitle: "Homenagem para professor (voz feminina)",
+        src: AudioFile1,
+      },
+      {
+        id: "1614",
+        subTitle: "Homenagem para professor (voz masculina)",
+        src: AudioFile2,
+      },
+      {
+        id: "1615",
+        subTitle: "Homenagem para professor (voz feminina)",
+        src: AudioFile3,
+      },
+      {
+        id: "1616",
+        subTitle: "Homenagem para professor (voz masculina)",
+        src: AudioFile4,
+      },
+      {
+        id: "1617",
+        subTitle: "Homenagem para professora (voz feminina)",
+        src: AudioFile5,
+      },
+      {
+        id: "1618",
+        subTitle: "Homenagem para professora (voz masculina)",
+        src: AudioFile6,
+      },
+      {
+        id: "1619",
+        subTitle: "Homenagem para professora (voz feminina)",
+        src: AudioFile7,
+      },
+      {
+        id: "1620",
+        subTitle: "Homenagem para professora (voz masculina)",
+        src: AudioFile8,
+      },
+      {
+        id: "1621",
+        subTitle: "Homenagem para professor (A) (voz feminina)",
+        src: AudioFile9,
+      },
+      {
+        id: "1622",
+        subTitle: "Homenagem para professor (A) (voz masculina)",
+        src: AudioFile10,
+      },
+      {
+        id: "1623",
+        subTitle: "Homenagem para professor (A) (voz feminina)",
+        src: AudioFile11,
+      },
+    ],
+  },
+];
--- src/routes/DatasEspeciais/DiaDosProfessores/DiaDosProfessores.tsx
+++ src/routes/DatasEspeciais/DiaDosProfessores/DiaDosProfessores.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import PageAudioLayout from "../../../components/PageAudioLayout/PageAudioLayout";
+import { audioData } from "./DiaDosProfessores.data";
 
-const DiaDosProfessores = () => {
-  return (
-    <div>DiaDosProfessores</div>
-  )
+const DiaDosProfessores: React.FC = () => {
+    return <PageAudioLayout audioData={audioData}/>
 }
 
-export default DiaDosProfessores
+export default DiaDosProfessores;
+
