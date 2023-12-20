52ea55fd6507542dd84836e67b0514e82b82fe6f
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-20 15:12:51 +0000

    Update Audio Data

--- src/routes/DatasEspeciais/DiaDoAmigo/DiaDoAmigo.data.ts
+++ src/routes/DatasEspeciais/DiaDoAmigo/DiaDoAmigo.data.ts
@@ -13,34 +13,6 @@ import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia
 import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-do-amigo/Telemensagem(6).mp3";
 // @ts-ignore
 import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-do-amigo/Telemensagem(7).mp3";
-// @ts-ignore
-// @ts-ignore
- //import AudioFile8 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(9).mp3";
- // @ts-ignore
- //import AudioFile9 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(10).mp3";
- // @ts-ignore
- //import AudioFile10 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(11).mp3";
- //// @ts-expect-error
- //import AudioFile12 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(12).mp3";
-// // @ts-ignore
-// import AudioFile13 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(13).mp3";
-// // @ts-ignore
-// import AudioFile14 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(14).mp3";
-// // @ts-ignore
-// import AudioFile15 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(15).mp3";
-// // @ts-ignore
-// import AudioFile16 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(16).mp3";
-// // @ts-ignore
-// import AudioFile17 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(17).mp3";
-// // @ts-ignore
-// import AudioFile18 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(18).mp3";
-// // @ts-ignore
-// import AudioFile19 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(19).mp3";
-// // @ts-ignore
-// import AudioFile20 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(20).mp3";
-// // @ts-ignore
-// import AudioFile21 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(21).mp3";
-
 export const audioData = [
     {
       id: "00",
@@ -82,9 +54,7 @@ export const audioData = [
           subTitle: "Dia do amigo para amiga (voz masculina)",
           src: AudioFile7,
         },
-        // Se houver mais subItems para adicionar com base nos seus imports, você pode continuar aqui.
       ],
     },
-    // Se houver mais itens para adicionar com base nos seus imports, você pode continuar aqui.
   ];
   
---
+++ src/routes/DatasEspeciais/DiaDoPais/DiaDosPais.data.ts
@@ -0,0 +1,180 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-expect-error
+import AudioFile1 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(1).mp3";
+// @ts-expect-error
+import AudioFile2 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(2).mp3";
+// @ts-expect-error
+import AudioFile3 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(3).mp3";
+// @ts-expect-error
+import AudioFile4 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(4).mp3";
+// @ts-expect-error
+import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(5).mp3";
+// @ts-expect-error
+import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(6).mp3";
+// @ts-expect-error
+import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(7).mp3";
+// @ts-expect-error
+import AudioFile8 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(8).mp3";
+// @ts-expect-error
+import AudioFile9 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(9).mp3";
+// @ts-expect-error
+import AudioFile10 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(10).mp3";
+// @ts-expect-error
+import AudioFile11 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(11).mp3";
+// @ts-expect-error
+import AudioFile12 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(12).mp3";
+// @ts-expect-error
+import AudioFile13 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(13).mp3";
+// @ts-expect-error
+import AudioFile14 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(14).mp3";
+// @ts-expect-error
+import AudioFile15 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(15).mp3";
+// @ts-expect-error
+import AudioFile16 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(16).mp3";
+// @ts-expect-error
+import AudioFile17 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(17).mp3";
+// @ts-expect-error
+import AudioFile18 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(18).mp3";
+// @ts-expect-error
+import AudioFile19 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(19).mp3";
+// @ts-expect-error
+import AudioFile20 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(20).mp3";
+// @ts-expect-error
+import AudioFile21 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(21).mp3";
+// @ts-expect-error
+import AudioFile22 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(22).mp3";
+// @ts-expect-error
+import AudioFile23 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(23).mp3";
+// @ts-expect-error
+import AudioFile24 from "../../../assets/Msgaudios/DataEspecial/Data-especial-dia-dos-pais/Telemensagem(24).mp3";
+
+export const audioData = [
+  {
+    id: "00",
+    title: "Dia dos Pais",
+    subTitle:
+      "Pai no Singular/ Pai no Plural/ Pai Distante/ Homenagem Para um Pai/Para Pai com Voz Infantil/ Pai de Consideração/ Pai Evangélico/ Para Avô/ Para Marido/ Para Namorado/ Para Sogro ",
+    subItems: [
+      {
+        id: "001 Pai",
+        subTitle: "Audio Dia dos Pais 1",
+        src: AudioFile1,
+      },
+      {
+        id: "002 Pai",
+        subTitle: "Audio Dia dos Pais 2",
+        src: AudioFile2,
+      },
+      {
+        id: "003 Pai",
+        subTitle: "Audio Dia dos Pais 3",
+        src: AudioFile3,
+      },
+      {
+        id: "004 Pai",
+        subTitle: "Audio Dia dos Pais 4",
+        src: AudioFile4,
+      },
+      {
+        id: "005 Pai",
+        subTitle: "Audio Dia dos Pais 5",
+        src: AudioFile5,
+      },
+      {
+        id: "006 Pai",
+        subTitle: "Audio Dia dos Pais 6",
+        src: AudioFile6,
+      },
+      {
+        id: "007 Pai",
+        subTitle: "Audio Dia dos Pais 7",
+        src: AudioFile7,
+      },
+      {
+        id: "008 Pai",
+        subTitle: "Audio Dia dos Pais 8",
+        src: AudioFile8,
+      },
+      {
+        id: "009 Pai",
+        subTitle: "Audio Dia dos Pais 9",
+        src: AudioFile9,
+      },
+      {
+        id: "010 Pai",
+        subTitle: "Audio Dia dos Pais 10",
+        src: AudioFile10,
+      },
+      {
+        id: "011 Pai",
+        subTitle: "Audio Dia dos Pais 11",
+        src: AudioFile11,
+      },
+      {
+        id: "012 Pai",
+        subTitle: "Audio Dia dos Pais 12",
+        src: AudioFile12,
+      },
+      {
+        id: "013 Pai",
+        subTitle: "Audio Dia dos Pais 13",
+        src: AudioFile13,
+      },
+      {
+        id: "014 Pai",
+        subTitle: "Audio Dia dos Pais 14",
+        src: AudioFile14,
+      },
+      {
+        id: "015 Pai",
+        subTitle: "Audio Dia dos Pais 15",
+        src: AudioFile15,
+      },
+      {
+        id: "016 Pai",
+        subTitle: "Audio Dia dos Pais 16",
+        src: AudioFile16,
+      },
+      {
+        id: "017 Pai",
+        subTitle: "Audio Dia dos Pais 17",
+        src: AudioFile17,
+      },
+      {
+        id: "018 Pai",
+        subTitle: "Audio Dia dos Pais 18",
+        src: AudioFile18,
+      },
+      {
+        id: "019 Pai",
+        subTitle: "Audio Dia dos Pais 19",
+        src: AudioFile19,
+      },
+      {
+        id: "020 Pai",
+        subTitle: "Audio Dia dos Pais 20",
+        src: AudioFile20,
+      },
+      {
+        id: "021 Pai",
+        subTitle: "Audio Dia dos Pais 21",
+        src: AudioFile21,
+      },
+      {
+        id: "022 Pai",
+        subTitle: "Audio Dia dos Pais 22",
+        src: AudioFile22,
+      },
+      {
+        id: "023 Pai",
+        subTitle: "Audio Dia dos Pais 23",
+        src: AudioFile23,
+      },
+      {
+        id: "024 Pai",
+        subTitle: "Audio Dia dos Pais 24",
+        src: AudioFile24,
+      },
+    ],
+  },
+];
