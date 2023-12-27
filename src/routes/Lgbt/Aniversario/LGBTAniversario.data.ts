9dc9a00b242e1e760451754d0aba8f3fc5686d2e
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-12-27 12:08:37 -0300

    Update Audio Data

---
+++ src/routes/Lgbt/Aniversario/LGBTAniversario.data.ts
@@ -0,0 +1,41 @@
+import AudioFile1 from "../../../assets/Msgaudios/LGBT/LGBT-aniversario-romantico/Telemensagem(1).mp3";
+import AudioFile2 from "../../../assets/Msgaudios/LGBT/LGBT-aniversario-romantico/Telemensagem(2).mp3";
+import AudioFile3 from "../../../assets/Msgaudios/LGBT/LGBT-aniversario-romantico/Telemensagem(3).mp3";
+import AudioFile4 from "../../../assets/Msgaudios/LGBT/LGBT-aniversario-romantico/Telemensagem(4).mp3";
+import AudioFile5 from "../../../assets/Msgaudios/LGBT/LGBT-aniversario-romantico/Telemensagem(5).mp3";
+
+
+export const audioData = [
+  {
+    id: "00",
+    title: "LGBT - Aniversário",
+    subTitle: "",
+    subItems: [
+      {
+        id: "001",
+        subTitle: "LGBT - Aniversário",
+        src: AudioFile1,
+      },
+      {
+        id: "002",
+        subTitle: "LGBT - Aniversário",
+        src: AudioFile2,
+      },
+      {
+        id: "003",
+        subTitle: "LGBT - Aniversário",
+        src: AudioFile3,
+      },
+      {
+        id: "004",
+        subTitle: "LGBT - Aniversário",
+        src: AudioFile4,
+      },
+      {
+        id: "005",
+        subTitle: "LGBT - Aniversário",
+        src: AudioFile5,
+      },
+    ],
+  },
+];
--- src/routes/Lgbt/Aniversario/LGBTAniversario.tsx
+++ src/routes/Lgbt/Aniversario/LGBTAniversario.tsx
@@ -1,7 +1,8 @@
+import PageAudioLayout from "../../../components/PageAudioLayout/PageAudioLayout";
+import { audioData } from "./LGBTAniversario.data";
+
 const LGBTAniversario: React.FC = () => {
-    return <>
-    building...
-    </>
+    return <PageAudioLayout audioData={audioData} />;
 };
 
 export default LGBTAniversario;
