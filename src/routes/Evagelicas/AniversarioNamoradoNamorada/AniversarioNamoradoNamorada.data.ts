0cb8456f17d31eef378e986a97625c09d991aa20
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-12-27 11:06:06 -0300

    Update Audio Data

--- src/routes/Evagelicas/AniversarioNamoradoNamorada/AniversarioNamoradoNamorada.data.ts
+++ src/routes/Evagelicas/AniversarioNamoradoNamorada/AniversarioNamoradoNamorada.data.ts
@@ -1,33 +1,27 @@
-import AudioFile1 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-mae/Telemensagem(1).mp3";
-import AudioFile2 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-mae/Telemensagem(2).mp3";
-import AudioFile3 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-mae/Telemensagem(3).mp3";
-import AudioFile4 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-mae/Telemensagem(4).mp3";
+import AudioFile1 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-Namorado-e-Namorada/Telemensagem(1).mp3";
+import AudioFile2 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-Namorado-e-Namorada/Telemensagem(2).mp3";
+import AudioFile3 from "../../../assets/Msgaudios/Evangelica/Evangelica-Aniversario-de-Namorado-e-Namorada/Telemensagem(3).mp3";
 
 export const audioData = [
   {
     id: "00",
-    title: "Aniversário de Mãe Evangélica",
+    title: "Aniversário de Namorado e Namorada - Evangélica",
     subTitle: "",
     subItems: [
       {
         id: "001",
-        subTitle: "Aniversário de Mãe Evangélica",
+        subTitle: "Aniversário de Namorado e Namorada - Evangélica",
         src: AudioFile1,
       },
       {
         id: "002",
-        subTitle: "Aniversário de Mãe Evangélica",
+        subTitle: "Aniversário de Namorado e Namorada - Evangélica",
         src: AudioFile2,
       },
       {
         id: "003",
-        subTitle: "Aniversário de Mãe Evangélica",
+        subTitle: "Aniversário de Namorado e Namorada - Evangélica",
         src: AudioFile3,
-      },
-      {
-        id: "004",
-        subTitle: "Aniversário de Mãe Evangélica",
-        src: AudioFile4,
       },
     ],
   },
