ec27bdcc4af17575a9a407aa665f8d0bbcb2c99e
Portela03 <joao.portela03@gmail.com>
2023-12-27 21:13:32 -0300

    subindo audios

--- src/routes/Variadas/DesculpaNeutra/DesculpasNeutra.data.ts
+++ src/routes/Variadas/DesculpaNeutra/DesculpasNeutra.data.ts
@@ -1,52 +1,70 @@
-import AudioFile1 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(1).mp3";
-import AudioFile2 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(2).mp3";
-import AudioFile3 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(3).mp3";
-import AudioFile4 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(4).mp3";
-import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(5).mp3";
-import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(6).mp3";
-import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(7).mp3";
+import AudioFile1 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(1).mp3";
+import AudioFile2 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(2).mp3";
+import AudioFile3 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(3).mp3";
+import AudioFile4 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(4).mp3";
+import AudioFile5 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(5).mp3";
+import AudioFile6 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(6).mp3";
+import AudioFile7 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(7).mp3";
+import AudioFile8 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(8).mp3";
+import AudioFile9 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(9).mp3";
+import AudioFile10 from "../../../assets/Msgaudios/Variadas/Variada-desculpas-neutras/Telemensagem(10).mp3";
 
 export const audioData = [
     {
       id: "00",
-      title: "Ano Novo",
-      subTitle: "(neutra/distante/namorados/casados/evangélica)",
+      title: "Desculpa Neutra",
+      subTitle: "",
       subItems: [
         {
-          id: "1154",
-          subTitle: " Ano Novo Neutra (voz masculina)",
+          id: "0417",
+          subTitle: "Desculpa neutra (voz feminina)",
           src: AudioFile1,
         },
         {
-          id: "0002",
-          subTitle: " 1154-Ano Novo Neutra (voz feminina)",
+          id: "0418",
+          subTitle: "Desculpa neutra (voz masculina)",
           src: AudioFile2,
         },
         {
-          id: "0003 M",
-          subTitle: " Aniversário amiga (voz feminina)",
+          id: "0419",
+          subTitle: "Desculpa neutra (voz feminina)",
           src: AudioFile3,
         },
         {
-          id: "0004 M",
-          subTitle: " Ano Novo Neutra (voz feminina)",
+          id: "0420",
+          subTitle: "Desculpa neutra (voz masculina)",
           src: AudioFile4,
         },
         {
-          id: "0657 M",
-          subTitle: " Ano Novo Neutra (voz feminina)",
+          id: "0421",
+          subTitle: "Desculpa neutra (voz feminina)",
           src: AudioFile5,
         },
         {
-          id: "0658 M",
-          subTitle: " Aniversário amiga (voz masculina)",
+          id: "0422",
+          subTitle: "Desculpa neutra (voz masculina)",
           src: AudioFile6,
         },
         {
-          id: "0005 M",
-          subTitle: " Ano Novo Neutra (voz masculina)",
+          id: "1689M",
+          subTitle: "Desculpa neutra (voz feminina)",
           src: AudioFile7,
         },
-    ]
-   }
- ];
+        {
+          id: "1690M",
+          subTitle: "Desculpa neutra (voz masculina)",
+          src: AudioFile8,
+        },
+        {
+          id: "0423",
+          subTitle: "Desculpa neutra (voz feminina)",
+          src: AudioFile9,
+        },
+        {
+          id: "0424",
+          subTitle: "Desculpa neutra (voz masculina)",
+          src: AudioFile10,
+        }
+      ]
+    }
+];
--- src/routes/Variadas/Formatura/Formatura.data.tsx
+++ src/routes/Variadas/Formatura/Formatura.data.tsx
@@ -1,52 +1,52 @@
-import AudioFile1 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(1).mp3";
-import AudioFile2 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(2).mp3";
-import AudioFile3 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(3).mp3";
-import AudioFile4 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(4).mp3";
-import AudioFile5 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(5).mp3";
-import AudioFile6 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(6).mp3";
-import AudioFile7 from "../../../assets/Msgaudios/DataEspecial/Data-especial-ano-novo/Telemensagem(7).mp3";
+import AudioFile1 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(1).mp3";
+import AudioFile2 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(2).mp3";
+import AudioFile3 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(3).mp3";
+import AudioFile4 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(4).mp3";
+import AudioFile5 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(5).mp3";
+import AudioFile6 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(6).mp3";
+import AudioFile7 from "../../../assets/Msgaudios/Variadas/Variada-formatura/Telemensagem(7).mp3";
 
 export const audioData = [
     {
       id: "00",
-      title: "Ano Novo",
-      subTitle: "(neutra/distante/namorados/casados/evangélica)",
+      title: "Formatura",
+      subTitle: "",
       subItems: [
         {
-          id: "1154",
-          subTitle: " Ano Novo Neutra (voz masculina)",
+          id: "1388",
+          subTitle: "formatura (voz feminina)",
           src: AudioFile1,
         },
         {
-          id: "0002",
-          subTitle: " 1154-Ano Novo Neutra (voz feminina)",
+          id: "1389",
+          subTitle: "formatura (voz masculina)",
           src: AudioFile2,
         },
         {
-          id: "0003 M",
-          subTitle: " Aniversário amiga (voz feminina)",
+          id: "1390",
+          subTitle: "formatura (voz feminina)",
           src: AudioFile3,
         },
         {
-          id: "0004 M",
-          subTitle: " Ano Novo Neutra (voz feminina)",
+          id: "1391",
+          subTitle: "formatura (voz masculina)",
           src: AudioFile4,
         },
         {
-          id: "0657 M",
-          subTitle: " Ano Novo Neutra (voz feminina)",
+          id: "1392",
+          subTitle: "formatura (voz feminina)",
           src: AudioFile5,
         },
         {
-          id: "0658 M",
-          subTitle: " Aniversário amiga (voz masculina)",
+          id: "1393",
+          subTitle: "formatura (voz masculina)",
           src: AudioFile6,
         },
         {
-          id: "0005 M",
-          subTitle: " Ano Novo Neutra (voz masculina)",
+          id: "1394",
+          subTitle: "formatura (voz feminina)",
           src: AudioFile7,
-        },
-    ]
-   }
- ];
+        }
+      ]
+    }
+];
