abaf40df3df96efe0c3c7493b19dbb2fc7ce82d8
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-04 20:31:23 -0300

    Update Audio Data

---
+++ src/routes/Aniversario/Aniversario-familiar/NetaNeto/NetaNeto.data.ts
@@ -0,0 +1,94 @@
+import AudioFile1 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(1).mp3";
+import AudioFile2 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(2).mp3";
+import AudioFile3 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(3).mp3";
+import AudioFile4 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(4).mp3";
+import AudioFile5 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(5).mp3";
+import AudioFile6 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(6).mp3";
+import AudioFile7 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(7).mp3";
+import AudioFile8 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(8).mp3";
+import AudioFile9 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(9).mp3";
+import AudioFile10 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(10).mp3";
+import AudioFile11 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(11).mp3";
+import AudioFile12 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(12).mp3";
+import AudioFile13 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(13).mp3";
+import AudioFile14 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-neto-e-neta/Telemensagem(14).mp3";
+
+export const audioData = [
+  {
+    id: "00",
+    title: "Aniversário Neta/Neto",
+    subTitle: "",
+    subItems: [
+      {
+        id: "0001",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile1,
+      },
+      {
+        id: "0002",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile2,
+      },
+      {
+        id: "0003",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile3,
+      },
+      {
+        id: "0004",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile4,
+      },
+      {
+        id: "0005",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile5,
+      },
+      {
+        id: "0006",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile6,
+      },
+      {
+        id: "0007",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile7,
+      },
+      {
+        id: "0008",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile8,
+      },
+      {
+        id: "0009",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile9,
+      },
+      {
+        id: "0010",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile10,
+      },
+      {
+        id: "0011",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile11,
+      },
+      {
+        id: "0012",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile12,
+      },
+      {
+        id: "0013",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile13,
+      },
+      {
+        id: "0014",
+        subTitle: "Aniversário Neta/Neto",
+        src: AudioFile14,
+      },
+    ],
+  },
+];
