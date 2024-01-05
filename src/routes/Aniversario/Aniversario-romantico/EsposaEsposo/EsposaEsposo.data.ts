e4e4b60eb4826fab19a6a712fb6dcdc973dccea9
Portela03 <joao.portela03@gmai.com>
2024-01-05 14:32:17 -0300

    Update audio data

--- src/routes/Aniversario/Aniversario-romantico/EsposaEsposo/EsposaEsposo.data.ts
+++ src/routes/Aniversario/Aniversario-romantico/EsposaEsposo/EsposaEsposo.data.ts
@@ -1,70 +1,72 @@
-import AudioFile1 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(1).mp3";
-import AudioFile2 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(2).mp3";
-import AudioFile3 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(3).mp3";
-import AudioFile4 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(4).mp3";
-import AudioFile5 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(5).mp3";
-import AudioFile6 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(6).mp3";
-import AudioFile7 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(7).mp3";
-import AudioFile8 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(8).mp3";
-import AudioFile9 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(9).mp3";
-import AudioFile10 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(10).mp3";
+import AudioFile1 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(1).mp3";
+import AudioFile2 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(2).mp3";
+import AudioFile3 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(3).mp3";
+import AudioFile4 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(4).mp3";
+import AudioFile5 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(5).mp3";
+import AudioFile6 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(6).mp3";
+import AudioFile7 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(7).mp3";
+import AudioFile8 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(8).mp3";
+import AudioFile9 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(9).mp3";
+import AudioFile10 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-esposo-e-esposa/Telemensagem(10).mp3";
 
 export const audioData = [
   {
     id: "00",
-    title: "Aniversário Afilhado/Afilhada",
+    title: "Aniversário de Esposa e Esposo",
     subTitle: "",
     subItems: [
       {
-        id: "0001",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0166",
+        subTitle: "Aniversário de esposo (voz feminina)",
         src: AudioFile1,
       },
       {
-        id: "0002",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0167",
+        subTitle: "Aniversário de esposa (voz masculina)",
         src: AudioFile2,
       },
       {
-        id: "0003",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "1268M",
+        subTitle: "Aniversário de esposo (voz feminina)",
         src: AudioFile3,
       },
       {
-        id: "0004",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "1269M",
+        subTitle: "Aniversário de esposa (voz masculina)",
         src: AudioFile4,
       },
       {
-        id: "0005",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "1270M",
+        subTitle: "Aniversário de esposo (voz feminina)",
         src: AudioFile5,
       },
       {
-        id: "0006",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "1271M",
+        subTitle: "Aniversário de esposa (voz masculina)",
         src: AudioFile6,
       },
       {
-        id: "0007",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0168",
+        subTitle: "Aniversário de esposo (voz feminina)",
         src: AudioFile7,
       },
       {
-        id: "0008",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0169",
+        subTitle: "Aniversário de esposa (voz masculina)",
         src: AudioFile8,
       },
       {
-        id: "0009",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0170",
+        subTitle: "Aniversário de esposo (voz feminina)",
         src: AudioFile9,
       },
       {
-        id: "0010",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0171",
+        subTitle: "Aniversário de esposa (voz masculina)",
         src: AudioFile10,
       },
+      
     ],
   },
 ];
+
--- src/routes/Aniversario/Aniversario-romantico/NamoradoNamorada/NamoradoNamorada.data.ts
+++ src/routes/Aniversario/Aniversario-romantico/NamoradoNamorada/NamoradoNamorada.data.ts
@@ -1,70 +1,68 @@
-import AudioFile1 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(1).mp3";
-import AudioFile2 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(2).mp3";
-import AudioFile3 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(3).mp3";
-import AudioFile4 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(4).mp3";
-import AudioFile5 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(5).mp3";
-import AudioFile6 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(6).mp3";
-import AudioFile7 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(7).mp3";
-import AudioFile8 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(8).mp3";
-import AudioFile9 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(9).mp3";
-import AudioFile10 from "../../../../assets/Msgaudios/Aniversario-familiar/Aniversario-familiar-afilhado-e-afilhada/Telemensagem(10).mp3";
+import AudioFile1 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(1).mp3";
+import AudioFile2 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(2).mp3";
+import AudioFile3 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(3).mp3";
+import AudioFile4 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(4).mp3";
+import AudioFile5 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(5).mp3";
+import AudioFile6 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(6).mp3";
+import AudioFile7 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(7).mp3";
+import AudioFile8 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(8).mp3";
+import AudioFile9 from "../../../../assets/Msgaudios/Aniversario-romantico/Aniversario-romantico-namorado-e-namorada/Telemensagem(9).mp3";
+
 
 export const audioData = [
   {
     id: "00",
-    title: "Aniversário Afilhado/Afilhada",
+    title: " Aniversário de Namorada/Namorado ",
     subTitle: "",
     subItems: [
       {
-        id: "0001",
-        subTitle: "Aniversário Afilhado/Afilhada",
+        id: "0665M",
+        subTitle: "Aniversário de namorado (voz feminina)",
         src: AudioFile1,
-      },
-      {
-        id: "0002",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0666M",
+        subTitle: "Aniversário de namorada (voz masculina)",
         src: AudioFile2,
-      },
-      {
-        id: "0003",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0667M",
+        subTitle: "Aniversário de namorado (voz feminina)",
         src: AudioFile3,
-      },
-      {
-        id: "0004",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0668M",
+        subTitle: "Aniversário de namorada (voz masculina)",
         src: AudioFile4,
-      },
-      {
-        id: "0005",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0669M",
+        subTitle: "Aniversário de namorado (voz feminina)",
         src: AudioFile5,
-      },
-      {
-        id: "0006",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0670M",
+        subTitle: "Aniversário de namorada (voz masculina)",
         src: AudioFile6,
-      },
-      {
-        id: "0007",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0671M",
+        subTitle: "Aniversário de namorado (voz feminina)",
         src: AudioFile7,
-      },
-      {
-        id: "0008",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0672M",
+        subTitle: "Aniversário de namorada (voz masculina)",
         src: AudioFile8,
-      },
-      {
-        id: "0009",
-        subTitle: "Aniversário Afilhado/Afilhada",
+    },
+    {
+        id: "0673M",
+        subTitle: "Aniversário de namorado (voz feminina)",
         src: AudioFile9,
-      },
-      {
-        id: "0010",
-        subTitle: "Aniversário Afilhado/Afilhada",
-        src: AudioFile10,
-      },
+    },
+    
+    
+      
     ],
   },
 ];
